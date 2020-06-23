12f452b281a22723316ff20d4a12d00f00b7e9da
Matt McNaney <mcnaneym@appstate.edu>
2020-06-23 11:19:50 -0400

    Fixed #8 - session change from history screen

--- javascript/RegistrationHistory/index.jsx
+++ javascript/RegistrationHistory/index.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import Waiting from '@essappstate/react-waiting'
-
+import Overlay from '@essappstate/canopy-react-overlay'
 /* global $, visitorId */
 
-const dateString = value => {
+const dateString = (value) => {
   const date = new Date(value * 1000)
   return date.toDateString()
 }
@@ -17,37 +17,96 @@ export default class RegistrationHistory extends React.Component {
     this.state = {
       listing: [],
       visitor: {},
+      show: false,
       loading: true,
-      message: null
+      message: null,
+      registration: {},
+      newSessionId: 0,
+      availableSessions: [],
     }
+    this.changeSession = this.changeSession.bind(this)
+    this.updateSession = this.updateSession.bind(this)
   }
   componentDidMount() {
     this.load()
+  }
+
+  loadAvailable() {
+    $.ajax({
+      url: 'conference/Admin/Session/available',
+      data: {
+        conferenceId: this.state.registration.conferenceId,
+        registrationId: this.state.registration.id,
+      },
+      dataType: 'json',
+      type: 'get',
+      success: (data) => {
+        if (data.listing !== null) {
+          this.setState({
+            availableSessions: data.listing,
+            newSessionId: data.listing[0].id,
+          })
+        }
+      },
+      error: () => {},
+    })
+  }
+
+  changeSession(key) {
+    const registration = this.state.listing[key]
+    this.setState({registration}, () => {
+      this.loadAvailable()
+      this.setState({show: true})
+    })
+  }
+
+  setNewSessionId(sessionId) {
+    this.setState({newSessionId: sessionId})
+  }
+
+  updateSession() {
+    $.ajax({
+      url: `conference/Admin/Registration/${this.state.registration.id}/changeSession`,
+      data: {sessionId: this.state.newSessionId},
+      dataType: 'json',
+      type: 'patch',
+      success: () => {
+        this.load()
+        this.setState({show: false})
+      },
+      error: () => {},
+    })
   }
 
   load() {
     $.ajax({
       url: './conference/Admin/Registration/visitor',
       data: {
-        visitorId: this.props.visitorId
+        visitorId: this.props.visitorId,
       },
       dataType: 'json',
       type: 'get',
-      success: data => {
+      success: (data) => {
         if (!data.listing) {
-          this.setState({message: 'Unable to connect', loading: false})
+          this.setState({
+            message: 'Unable to connect',
+            loading: false,
+          })
         } else {
           this.setState({
             listing: data.listing,
             loading: false,
             message: null,
-            visitor: data.visitor
+            visitor: data.visitor,
           })
         }
       },
       error: () => {
-        this.setState({message: 'Unable to connect', loading: false})
-      }
+        this.setState({
+          message: 'Unable to connect',
+          loading: false,
+        })
+      },
     })
   }
 
@@ -82,6 +141,16 @@ export default class RegistrationHistory extends React.Component {
                 </a>
                 <hr className="my-0" />
                 <a
+                  className="dropdown-item pointer"
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    this.changeSession(key)
+                  }}>
+                  <i className="fas fa-exchange-alt" />
+                  &nbsp;Change session
+                </a>
+                <a
                   href={`conference/Admin/Visitor/loginAs?visitorId=${value.visitorId}&amp;registrationId=${value.id}`}
                   className="dropdown-item">
                   <i className="fas fa-sign-in-alt" />
@@ -95,7 +164,8 @@ export default class RegistrationHistory extends React.Component {
                 <a
                   href={`./conference/Admin/Log/visitor/?vid=${value.visitorId}`}
                   className="dropdown-item pointer">
-                  <i className="fas fa-clipboard-list"></i>&nbsp;Log
+                  <i className="fas fa-clipboard-list"></i>
+                  &nbsp;Log
                 </a>
               </div>
             </div>
@@ -128,6 +198,7 @@ export default class RegistrationHistory extends React.Component {
 
   render() {
     let message
+    const {newSessionId} = this.state
     if (this.state.message !== null) {
       message = (
         <div className="alert alert-danger">
@@ -144,6 +215,30 @@ export default class RegistrationHistory extends React.Component {
     } else {
       content = this.getListing()
     }
+
+    const optionList = this.state.availableSessions.map((value) => {
+      return (
+        <option key={value.id} value={value.id}>
+          {value.eventDateFormat}&#32;&ndash;&#32;{value.title}
+        </option>
+      )
+    })
+
+    const options = (
+      <div>
+        <select
+          className="form-control"
+          value={this.state.newSessionId}
+          onChange={(e) => this.setNewSessionId(e.target.value)}>
+          {optionList}
+        </select>
+        <hr />
+        <button className="btn btn-success" onClick={this.updateSession}>
+          Update
+        </button>
+      </div>
+    )
+
     return (
       <div>
         <h2>
@@ -152,13 +247,20 @@ export default class RegistrationHistory extends React.Component {
         </h2>
         {message}
         <div>{content}</div>
+        <Overlay
+          show={this.state.show}
+          close={() => this.setState({show: false})}
+          width="500px"
+          title="My Overlay">
+          {options}
+        </Overlay>
       </div>
     )
   }
 }
 
 RegistrationHistory.propTypes = {
-  visitorId: PropTypes.number
+  visitorId: PropTypes.number,
 }
 
 ReactDOM.render(
