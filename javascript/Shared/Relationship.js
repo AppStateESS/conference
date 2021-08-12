6749864e675549fd45d5d65535250f2320f99d84
Matt McNaney <mcnaneym@appstate.edu>
2021-08-12 11:02:43 -0400

    Relation list file added.

---
+++ javascript/Shared/Relationship.js
@@ -0,0 +1,12 @@
+export const relationships = [
+  'Mother',
+  'Father',
+  'Parent',
+  'Grandfather',
+  'Grandmother',
+  'Grandparent',
+  'Inlaw',
+  'Step-parent',
+  'Guardian',
+  'Other'
+]
