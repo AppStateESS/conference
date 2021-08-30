a5f649151364ebbcb7a2e7e10b0343091785b1f9
Matt McNaney <mcnaneym@appstate.edu>
2021-08-30 10:20:37 -0400

    Added CONFERENCE_SWIFT_OLD_VERSION define.

--- config/sendmail.dist.php
+++ config/sendmail.dist.php
@@ -10,3 +10,12 @@
  * @license https://opensource.org/licenses/MIT
  */
 define('CONFERENCE_SENDMAIL_COMMAND', '/usr/sbin/sendmail -t -i');
+
+/**
+ * If SwiftMailer is less than version 6, set this to true.
+ * Version 5 has a different way of instantiating objects.
+ * Find out which you have with:
+ * > composer show swiftmailer/swiftmailer
+ * 
+ */
+define('CONFERENCE_SWIFT_OLD_VERSION', false);
